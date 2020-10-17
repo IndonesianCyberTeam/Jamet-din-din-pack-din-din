8bd60f1653344d036c4c1e1f00a09727b32cd89a
Indonesian Cyber Team <70948716+IndonesianCyberTeam@users.noreply.github.com>
2020-10-17 15:04:46 +0800

    Update jamet tukang.php

--- jamet tukang.php
+++ jamet tukang.php
@@ -1,9 +1,10 @@
 <?php
 <html>
 <head><title>jamet gans</title></head>
+<!--Jamet{Flagnya-eyy}-->
 
 
-
+3C 3F 70 68 70 3C 68 74 6D 6C 3E 3C 68 65 61 64 3E 3C 74 69 74 6C 65 3E 6A 61 6D 65 74 20 67 61 6E 73 3C 2F 74 69 74 6C 65 3E 3C 2D 2D 4A 61 6D 65 74 7B 46 6C 61 67 2D 45 65 65 79 7D 2D 2D 3E 3C 2F 68 65 61 64 3E 20 3C 2F 68 74 6D 6C 3E 3F 3E
 
 
 
